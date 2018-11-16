854384017b7bdc10c0e8de4538db2af24433fa76
yanhh <yanhh@wecut.com>
2018-11-16 12:05:03 +0800

    master 添加获取系统信息

--- app/src/main/java/com/hao/myutils/util/InfoUtil.java
+++ app/src/main/java/com/hao/myutils/util/InfoUtil.java
@@ -1,8 +1,14 @@
 package com.hao.myutils.util;
 
 import android.os.Build;
+import android.os.Environment;
+import android.os.StatFs;
+import android.os.SystemClock;
+import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -28,4 +34,104 @@ public class InfoUtil {
         return Build.HARDWARE;
     }
 
+    // 实时获取CPU当前频率（单位KHZ）
+    public static String getCurCpuFreq() {
+        String result = "N/A";
+        try {
+            FileReader fr = new FileReader(
+                    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq");
+            BufferedReader br = new BufferedReader(fr);
+            String text = br.readLine();
+            result = text.trim();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void logMemoryInfo() {
+        String str1 = "/proc/meminfo";
+        String str2;
+        try {
+            FileReader fr = new FileReader(str1);
+            BufferedReader localBufferedReader = new BufferedReader(fr, 8192);
+            while ((str2 = localBufferedReader.readLine()) != null) {
+                Log.e("memoryInfo", "---" + str2);
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    // 获取rom大小
+    public static long[] getRomMemroy() {
+        long[] romInfo = new long[2];
+        romInfo[0] = getTotalInternalMemorySize();
+
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long availableBlocks = stat.getAvailableBlocks();
+        romInfo[1] = blockSize * availableBlocks;
+        return romInfo;
+    }
+
+    public static long getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long totalBlocks = stat.getBlockCount();
+        return totalBlocks * blockSize;
+    }
+
+    // 获取SDCard大小
+    public static long[] getSDCardMemory() {
+        long[] sdCardInfo=new long[2];
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            File sdcardDir = Environment.getExternalStorageDirectory();
+            StatFs sf = new StatFs(sdcardDir.getPath());
+            long bSize = sf.getBlockSize();
+            long bCount = sf.getBlockCount();
+            long availBlocks = sf.getAvailableBlocks();
+
+            sdCardInfo[0] = bSize * bCount;//总大小
+            sdCardInfo[1] = bSize * availBlocks;//可用大小
+        }
+        return sdCardInfo;
+    }
+
+    // 获取系统版本信息
+    public static String[] getVersion(){
+        String[] version={"null","null","null","null"};
+        String str1 = "/proc/version";
+        String str2;
+        String[] arrayOfString;
+        try {
+            FileReader localFileReader = new FileReader(str1);
+            BufferedReader localBufferedReader = new BufferedReader(
+                    localFileReader, 8192);
+            str2 = localBufferedReader.readLine();
+            arrayOfString = str2.split("\\s+");
+            version[0]=arrayOfString[2];//KernelVersion
+            localBufferedReader.close();
+        } catch (IOException e) {
+        }
+        version[1] = Build.VERSION.RELEASE;// firmware version
+        version[2]=Build.MODEL;//model
+        version[3]=Build.DISPLAY;//system version
+        return version;
+    }
+
+    // 获取系统开机时间
+    public static String getTimes() {
+        long ut = SystemClock.elapsedRealtime() / 1000;
+        if (ut == 0) {
+            ut = 1;
+        }
+        int m = (int) ((ut / 60) % 60);
+        int h = (int) ((ut / 3600));
+        return h + " " + "h" + m + " " + "m";
+    }
 }
